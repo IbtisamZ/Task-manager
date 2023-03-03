a686c9bbdef18ed473fc65b17ed8348f066fecfc
IbtisamZ <ibtisam.alzahranii@gmail.com>
2023-03-03 16:45:03 +0300

    introduce mui to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import TodoListCard from './components/TodoListCard';
 
-const App = () => (
-  <div className="task-manager-app">
-    <TodoListCard />
-  </div>
-);
+const App = () => {
+  const theme = createTheme({
+    palette: {
+      background: {
+        default: '#fff',
+      },
+      primary: {
+        main: '#d7e6ea',
+      },
+      text: {
+        main: '#fff',
+      },
+      common: {
+        gray: '#808080',
+        water: '#cce9ee',
+        antiFlashWhite: '#eff1f2',
+        silverSand: '#b8c3c6',
+      },
+    },
+  });
+
+  return (
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <TodoListCard />
+      </ThemeProvider>
+  );
+};
 
 export default App;
